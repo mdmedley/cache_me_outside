8d205af06509e7003d3d5e04a764d96874a49d53
Marcus Medley <mdmeds@gmail.com>
2017-04-01 20:32:22 -0700

    Update keywords

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 from sys import version_info
 
+__version__ = '0.1'
 
 requirements = []
-# If Python version is lower than 3.3 require backports.functools_lru_cache
+# If Python version is lower than 3.2 require backports.functools_lru_cache
 if version_info < (3, 2):
     requirements = ['backports.functools_lru_cache']
 
-__version__ = '0.1'
-
-description = 'A package that wraps functools_lru_cache with a fun name'
+description = 'A package that makes caching fun! How about that?'
 
 
 setup(
@@ -24,7 +23,7 @@ setup(
     install_requires=[requirements],
     setup_requires=['pytest-runner'],
     tests_requires=['pytest'],
-    keywords=['functools', 'lru_cache', 'cache', 'python 2.7 cache'],
+    keywords=['cache', 'caching', 'python cache'],
     url='https://github.com/mdmedley/cache_me_outside',
     classifiers=(
         'Development Status :: 4 - Beta',
