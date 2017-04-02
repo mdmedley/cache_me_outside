5480edbd93566f3b8f57f6b6c5e0f0a7abaeb0cc
Marcus Medley <mdmeds@gmail.com>
2017-04-01 19:45:03 -0700

    Update setup.py to only require packages needed

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
+from sys import version_info
+
+
+requirements = []
+# If Python version is lower than 3.3 require backports.functools_lru_cache
+if version_info < (3, 2):
+    requirements = ['backports.functools_lru_cache']
 
 __version__ = '0.1'
 
@@ -14,7 +21,7 @@ setup(
     author_email='mdmeds@gmail.com',
     license='MIT',
     py_modules=['cache_me_outside'],
-    install_requires=['backports.functools_lru_cache', 'setuptools'],
+    install_requires=[requirements],
     setup_requires=['pytest-runner'],
     tests_requires=['pytest'],
     keywords=['functools', 'lru_cache', 'cache', 'python 2.7 cache'],
